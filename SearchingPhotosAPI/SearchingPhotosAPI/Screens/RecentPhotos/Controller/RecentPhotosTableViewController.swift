05d071ea2388887a72d29160d0f000a89fdfa30f
MuhammedGul34 <116461079+MuhammedGul34@users.noreply.github.com>
2022-12-28 23:53:06 +0300

    Fetch Request for Search and Recent Photos
    Creating Model for this project.

--- SearchingPhotosAPI/SearchingPhotosAPI/Screens/RecentPhotos/Controller/RecentPhotosTableViewController.swift
+++ SearchingPhotosAPI/SearchingPhotosAPI/Screens/RecentPhotos/Controller/RecentPhotosTableViewController.swift
@@ -8,19 +8,61 @@
 import UIKit
 
 class RecentPhotosTableViewController: UITableViewController, UISearchResultsUpdating {
-   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    
+    var response : PhotoResponse? {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    private func setupSearchController(){
         let search = UISearchController(searchResultsController: nil)
         search.searchResultsUpdater = self
         search.obscuresBackgroundDuringPresentation = false
         search.searchBar.placeholder = "Type here to search"
         navigationItem.searchController = search
         
+    }
+    
+    private func fetchRequest(){
+        guard let url = URL(string: "https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=de0b083d4a218ed778abc1dbdc6811e2&format=json&nojsoncallback=1&extras=description,owner_name,icon_server,url_n,url_z") else {return}
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                debugPrint(error)
+                return
+            }
+            if let data = data, let response = try? JSONDecoder().decode(PhotoResponse.self, from: data) {
+                self.response = response
+            }
+        }.resume()
+    }
+    
+    private func searchPhotos(with text: String){
+        guard let url = URL(string: "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=de0b083d4a218ed778abc1dbdc6811e2&text=\(text)&format=json&nojsoncallback=1&extras=description,owner_name,icon_server,url_n,url_z") else {return}
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                debugPrint(error)
+                return
+            }
+            if let data = data, let response = try? JSONDecoder().decode(PhotoResponse.self, from: data) {
+                self.response = response
+            }
+        }.resume()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        fetchRequest()
+        setupSearchController()
+        
         tableView.delegate = self
         tableView.dataSource = self
-        
+      
         self.tableView.register(UINib(nibName: "RecentPhotoCustomCell", bundle: nil), forCellReuseIdentifier: "cell")
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -53,7 +95,7 @@ class RecentPhotosTableViewController: UITableViewController, UISearchResultsUpd
     func updateSearchResults(for searchController: UISearchController) {
         guard let text = searchController.searchBar.text else { return }
         if text.count > 2 {
-            print(text)
+            searchPhotos(with: text)
         }
     }
 }
